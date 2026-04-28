97000bae43647fe63d0e3c5f3feadaae041a8f4b
Aljóna Đukić <github@alena.red>
2026-04-28 21:44:56 +0500

    build: ⬆️ upgrade deps

--- frontend/src/react-simple-maps.d.ts
+++ frontend/src/react-simple-maps.d.ts
@@ -1,5 +1,5 @@
 declare module 'react-simple-maps' {
-	import type { ComponentType, ReactNode, CSSProperties } from 'react';
+	import type { ComponentType, CSSProperties, ReactNode } from 'react';
 
 	interface ProjectionConfig {
 		rotate?: [number, number, number];
